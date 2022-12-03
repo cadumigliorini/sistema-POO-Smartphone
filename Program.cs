9cb2740ffc17732d9a606c2586dfe7d1623b1aba
Carlos Migliorini <cadumigliorini@hotmail.com>
2022-12-03 15:28:42 -0300

    projeto de estudos POO finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+
+Smartphone nokia = new Nokia(numero:"999999999", modelo:"modelo 1", imei: "1111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Smartphone iphone = new Iphone(numero:"111111111", modelo:"modelo 2", imei:"2222222222", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
